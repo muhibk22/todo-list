86c05c00f268ee5ca3e638813b1d3274af3deb4d
Muhib Khan <m.muhibk22@gmail.com>
2024-09-11 03:43:19 +0500

    Add a new task overlay

---
+++ src/add.js
@@ -0,0 +1,6 @@
+export default function addTask(){
+    const add=document.getElementById("add");
+    add.addEventListener("click",function(){
+        alert("hello")
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./styles.css";
+import "./form.css"
 import navdrop from "./nav";
+import addTask from "./add";
 
 document.addEventListener("DOMContentLoaded",function(){
     navdrop();
+    addTask();
 })
