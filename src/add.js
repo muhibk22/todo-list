e17fb0ec15e93cf77503547201ee5982bbcfae21
Muhib Khan <m.muhibk22@gmail.com>
2024-09-11 05:32:09 +0500

    add toggle display to forms

--- src/add.js
+++ src/add.js
@@ -1,6 +1,34 @@
 export default function addTask(){
     const add=document.getElementById("add");
+    const form=document.querySelector(".form-container");
+    const close=document.getElementById("close-form");
+    const task=document.getElementById("task");
+    const project=document.getElementById("project");
+    const taskForm=document.getElementById("todo-form");
+    const projectForm=document.getElementById("project-form");
+
+    close.addEventListener("click",function(){
+        form.classList.add("hide");
+    });
     add.addEventListener("click",function(){
-        alert("hello")
+        form.classList.remove("hide");
+    });
+
+    task.addEventListener("click",function(){
+        if (taskForm.classList.contains("hide")) {
+            taskForm.classList.remove("hide");
+            if (!projectForm.classList.contains("hide")){
+                projectForm.classList.add("hide");
+            }
+        }
+    });
+
+    project.addEventListener("click",function(){
+        if (projectForm.classList.contains("hide")) {
+            projectForm.classList.remove("hide");
+            if (!taskForm.classList.contains("hide")){
+                taskForm.classList.add("hide");
+            }
+        }
     })
 }
