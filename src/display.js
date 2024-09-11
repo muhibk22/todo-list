b82b819e54f0714a4e9ac98dd87715ad3e67b5e4
Muhib Khan <m.muhibk22@gmail.com>
2024-09-11 20:08:30 +0500

    make tasks through dom

--- src/display.js
+++ src/display.js
@@ -1,3 +1,6 @@
+import editImg from "./images/pencil.svg";
+import deleteImg from "./images/delete.svg";
+
 function updateProjects(projectArray){
     const projects=document.querySelector(".projects");
     const ul=document.createElement("ul");
@@ -9,4 +12,77 @@ function updateProjects(projectArray){
     }
 };
 
-export {updateProjects};
+function deleteTask(){
+    const deleteButton=document.querySelectorAll(".delete");
+    let index;
+    deleteButton.forEach(button=>{
+        button.addEventListener("click",()=>{
+            const taskDiv=button.closest(".tasks");
+            index=taskDiv.getAttribute("data-index");
+            taskDiv.remove();
+            console.log(index);
+        });
+    });
+};
+
+
+function updateTasks(taskArray){
+    const taskContainer=document.querySelector(".task-container");
+    if (taskArray.length===0){
+        taskContainer.appendChild(createTask(0,"Do laundry", "Sep 12"));
+        taskContainer.appendChild(createTask(1,"Go to the gym","Sep 12" ));
+    }
+    deleteTask();
+
+}
+
+function createTask(index,title,dueDate) {
+    
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('tasks');
+    taskDiv.setAttribute('data-index', index );
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+    taskDiv.appendChild(checkbox);
+
+    const taskTitleDiv = document.createElement('div');
+    taskTitleDiv.classList.add('task-title-div');
+    
+    
+    const taskTitle = document.createElement('p');
+    taskTitle.classList.add('task-title');
+    taskTitle.textContent = title;
+    taskTitleDiv.appendChild(taskTitle);
+    
+    taskDiv.appendChild(taskTitleDiv);
+
+    const detailsButton = document.createElement('button');
+    detailsButton.type = 'button';
+    detailsButton.classList.add('details');
+    detailsButton.textContent = 'Details';
+    taskDiv.appendChild(detailsButton);
+
+    const date = document.createElement('p');
+    date.classList.add('date');
+    date.textContent = dueDate;
+    taskDiv.appendChild(date);
+
+    
+    const editButton = document.createElement('img');
+    editButton.src = editImg;
+    editButton.alt = 'Edit';
+    editButton.classList.add('modify-btn', 'edit');
+    taskDiv.appendChild(editButton);
+
+    const deleteButton = document.createElement('img');
+    deleteButton.src = deleteImg;
+    deleteButton.alt = 'Delete';
+    deleteButton.classList.add('modify-btn', 'delete');
+    taskDiv.appendChild(deleteButton);
+
+    return taskDiv;
+}
+
+export {updateProjects,deleteTask,updateTasks};
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import "./styles.css";
 import "./form.css"
 import navdrop from "./nav";
 import addTask from "./add";
-import { submitTodo,submitProject } from "./submit";
-import { updateProjects } from "./display";
+import {submitTodo,submitProject } from "./submit";
+import {updateProjects ,updateTasks} from "./display";
 
 document.addEventListener("DOMContentLoaded",function(){
-    navdrop();
-    addTask();
     const todoBtn=document.getElementById("submit-todo");
     const projectBtn=document.getElementById("submit-project");
     const todoArray=[];
     const projectArray=[];
+
+    navdrop();
+    addTask();
     updateProjects(projectArray);
+    updateTasks(todoArray);
+
+
     todoBtn.addEventListener("click",()=>submitTodo(todoArray));
     projectBtn.addEventListener("click",()=>submitProject(projectArray));
 });
